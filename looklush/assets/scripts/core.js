993f1ee119eebf842a262f67af001e94bc3c0ccf
JR <dsgrtts@mail.com>
2021-02-07 16:50:28 -0500

    added looklush website directory.

---
+++ looklush/assets/scripts/core.js
@@ -0,0 +1,36 @@
+var nav,
+    menu,
+    menuButton;
+
+nav        = document.querySelector('nav');
+menu       = document.querySelector('.menu');
+menuButton = document.querySelector('.menu-button');
+
+window.onscroll = function () {
+  nav = document.querySelector('nav');
+  if (window.scrollY > 250) {
+    if (!nav.classList.contains('dark')) {
+      nav.className += ' dark';
+    }
+  } else {
+    nav.className = nav.className.replace(' dark', '');
+  }
+}
+
+menuButton.onclick = function() {
+  if (!menu.classList.contains('visible')) {
+    menu.classList.add('visible');
+  }
+}
+
+document.addEventListener('click', function (element) {
+  element = element.target;
+  console.log(element);
+  if (menu.classList.contains('visible')) {
+    if (!element.parentElement.classList.contains('menu')
+    &&  !element.parentElement.parentElement.classList.contains('menu')
+    &&  !element.classList.contains('menu-button')) {
+      menu.classList.remove('visible')
+    }
+  }
+});
